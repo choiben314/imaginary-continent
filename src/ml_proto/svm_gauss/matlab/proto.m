f80c9682aa23b0a7ca45d6c6e0749e5265504643
Ben Choi <choiben314@gmail.com>
2018-09-14 19:37:57 +0100

    use grid search to optimize C and gamma

--- src/ml_proto/svm_gauss/matlab/proto.m
+++ src/ml_proto/svm_gauss/matlab/proto.m
@@ -7,11 +7,11 @@ M = csvread(training_file);
 
 M = M(randperm(size(M, 1)), :);
 
-test_size = 10;
+test_size = 1000;
 
 idx = 1 + size(M, 1) - test_size;
 
-X = M(1:idx, 1:end);
+X = M(1:idx, 1:end - 1);
 y = M(1:idx, end);
 
 % training_labels = y(1:10000, :);
@@ -23,16 +23,46 @@ y = M(1:idx, end);
 training_labels = y;
 training_set = X;
 
-testing_set = M(idx:end, 1:end);
+testing_set = M(idx:end, 1:end - 1);
 testing_labels = M(idx:end, end);
+% testing_set = M(1:test_size, 1:end - 1);
+% testing_labels = M(1:test_size, end)
 
-model = svmtrain(training_labels, training_set, [, '-c 0.1 -g 70']);
+% highest = 0;
+% best_c = -8;
+% best_g = -8;
+
+% for c = -8:8
+% 	for g = -8:8
+% 		lib_opt = sprintf('-c %f -g %f', 3.162^-c, 3.162^-g)
+% 		model = svmtrain(training_labels, training_set, [, lib_opt]);
+% 		[predicted_label, accuracy, prob_estimates] = svmpredict(testing_labels, testing_set, model);
+% 		if accuracy(1) > highest
+% 			highest = accuracy(1);
+% 			best_c = c;
+% 			best_g = g;
+% 		disp(best_c);
+% 		disp(best_g);
+% 		disp(accuracy(1));
+% 		end
+% 	end
+% end
+
+model = svmtrain(training_labels, training_set, [, '-c 3.0 -g 70.0']);
 
 [predicted_label, accuracy, prob_estimates] = svmpredict(testing_labels, testing_set, model);
+
+% disp("~~~~~~~~best c value~~~~~~~~")
+% disp(best_c)
+% disp("~~~~~~~~best g value~~~~~~~~")
+% disp(best_g)
+% disp("~~~~~~~~best accuracy~~~~~~~~")
+% disp(highest)
+
 disp("-----Predicted Labels-----");
-disp(predicted_label);
+%disp(predicted_label);
 disp("-----Original Labels-----");
-disp(testing_labels);
+%disp(testing_labels);
 disp("-----Accuracy-----");
 disp(accuracy);
 
